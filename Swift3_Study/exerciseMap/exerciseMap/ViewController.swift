3fd0fea0973eada9ca04f14b4e92735c99b510ad
lmjing <lmjing1234@gmail.com>
2017-08-07 16:20:08 +0900

    현재위치 표시

--- Swift3_Study/exerciseMap/exerciseMap/ViewController.swift
+++ Swift3_Study/exerciseMap/exerciseMap/ViewController.swift
@@ -8,9 +8,11 @@
 
 import UIKit
 import GoogleMaps
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, GMSMapViewDelegate, CLLocationManagerDelegate {
 
+    var mapView: GMSMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -24,18 +26,32 @@ class ViewController: UIViewController {
     override func loadView() {
         // Create a GMSCameraPosition that tells the map to display the
         // coordinate -33.86,151.20 at zoom level 6.
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        let camera = GMSCameraPosition.camera(withLatitude: 37.5106336, longitude: 127.11220, zoom: 16.0)
+        mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         mapView.isMyLocationEnabled = true
         view = mapView
         
         // Creates a marker in the center of the map.
         let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.position = CLLocationCoordinate2D(latitude: 37.5106336, longitude: 127.112208)
         marker.title = "Sydney"
         marker.snippet = "Australia"
         marker.map = mapView
+        
     }
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let mylocation = mapView.myLocation {
+            print("User's location: \(mylocation)")
+            let lat = mylocation.coordinate.latitude
+            let long = mylocation.coordinate.longitude
+            print(lat,long)
+            let camera = GMSCameraPosition.camera(withLatitude: lat, longitude: long, zoom: 14.0)
+            mapView.camera = camera
+            mapView.animate(to: camera)
+        } else {
+            print("User's location is unknown")
+        }
+    }
 }
 
